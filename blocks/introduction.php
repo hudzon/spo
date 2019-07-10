6dfad86616d4ce7f738a1110e7881a7859402b98
Serg <hudzon01@mail.ru>
2019-07-10 12:46:47 +0300

    [WIP] Init

---
+++ blocks/introduction.php
@@ -0,0 +1,4 @@
+<div id="introduction">
+	Учебные и воспитательные цели:
+    <input type="text" size="40">
+</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>План занятий</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="container">
+	<?php include ("blocks/graph.php");?>
+	<?php include ("blocks/header.php");?>
+	<div id="content">
+    	<?php include ("blocks/introduction.php");?>
+    	<?php include ("blocks/question_table.php");?>
+	</div>
+</div>
+</body>
+</html>
